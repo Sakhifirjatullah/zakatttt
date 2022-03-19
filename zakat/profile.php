9268f7884d3ddc39e6b446cbf95128e2ee9f203d
Sakhifirjatullah <91299138+Sakhifirjatullah@users.noreply.github.com>
2022-03-20 06:30:48 +0700

    Add files via upload

---
+++ zakat/profile.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	    <meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <link href="css/bootstrap.min.css" rel="stylesheet">
+	    <link href="css/style.css" rel="stylesheet">
+   	<title>Profile Pembuat</title>
+  </head>
+  <body>
+  <div class="container">
+  	<div class="judul">
+		<h1>kelompok 1, PPLG X-4</h1>
+			1. Sakhi Firjatullah<br>
+     		2. Airin Dwi Ayumi<br>
+			3. Muhammad Saputra Ardiansyah Wiranata Hasibuan<br>
+			
+	</div>
+	<hr>
+	
+  </div>
+  </body>
+  </html>
---
+++ zakat/zakat.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	    <meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <link href="css/bootstrap.min.css" rel="stylesheet">
+	    <link href="css/style.css" rel="stylesheet">
+   	<title>Hitung Zakat</title>
+  </head>
+  <body>
+  <div class="container">
+  	<div class="judul">
+		<h1>Zakat</h1>
+			Mengutip penjelasan ahli tafsir Quraish Shihab dalam buku Tafsir Al Misbah, zakat berarti penyucian dan berkembang. Maksudnya, melalui pembayaran zakat, seorang Muslim diajak untuk menyucikan jiwa dengan mengikis sifat tamak, kikir, loba di dalam dirinya.<br><br>
+			Zakat adalah sebagian dari harta yang wajib dikeluarkan oleh seorang muslim untuk dibagikan kepada 8 golongan yang berhak menerima zakat.
+		<h3><b>Jenis Zakat</b></h3>
+			<h3><b>Secara garis besar, zakat dibagi 2 yaitu : </b></h3>
+			<ul>
+				<li><b>Zakat Fitrah</b></li>
+				<p>Zakat fitrah adalah zakat yang harus ditunaikan bagi seorang muzakki yang telah memiliki kemampuan untuk menunaikannya. Zakat fitrah adalah zakat wajib yang harus dikeluarkan sekali setahun yaitu saat bulan ramadhan menjelang idul fitri. Pada prinsipnya, zakat fitrah haruslah dikeluarkan sebelum sholat idul fitri dilangsungkan. Hal tersebut yang menjadi pembeda zakat fitrah dengan zakat lainnya.</p>
+				<li><b>Zakat Mal/Harta</b>
+				<p>Maal berasal dari kata bahasa Arab artinya harta atau kekayaan (al-amwal, jamak dari kata maal) adalah “segala hal yang diinginkan manusia untuk disimpan dan dimiliki” (Lisan ul-Arab). Menurut Islam sendiri, harta merupakan sesuatu yang boleh atau dapat dimiliki dan digunakan (dimanfaatkan) sesuai kebutuhannya.
+
+                Oleh karena itu dalam pengertiannya, zakat maal berarti zakat yang dikenakan atas segala jenis harta, yang secara zat maupun substansi perolehannya tidak bertentangan dengan ketentuan agama.</p>
+					<ul>
+						<li>Zakat Perdagangan</li>
+						<li>Zakat Pertanian</li>
+						<li>Zakat Hewan Ternak</li>
+						<li>Zakat Emas dan Perak</li>
+						<li>Zakat Profesi/Penghasilan</li>
+						<li>Zakat Investasi</li>
+						<li>Zakat Tabungan</li>
+						<li>Zakat Rikaz</li>
+					</ul>
+				</li>
+			</ul>
+	</div>
+  </div>
+  </body>
+  </html>
